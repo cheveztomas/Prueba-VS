df92d6f2dd3b8c029a95c4fa1a613f2e54470579
Juan Castro <sempai211@gmail.com>
2019-01-08 15:32:22 -0600

    Update FrmRegistroDeUsuarios.aspx.cs

--- DirectorioServicios/FrmRegistroDeUsuarios.aspx.cs
+++ DirectorioServicios/FrmRegistroDeUsuarios.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
+using EntidadesDirectorio;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using CapaLogica;
 
 namespace DirectorioServicios
 {
