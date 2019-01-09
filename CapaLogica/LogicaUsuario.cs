d85fe6d375e77c3741e9103e50f096280c6819f3
cheveztomas <42824002+cheveztomas@users.noreply.github.com>
2019-01-09 10:14:49 -0600

    Update LogicaUsuario.cs

--- CapaLogica/LogicaUsuario.cs
+++ CapaLogica/LogicaUsuario.cs
@@ -1,14 +1,13 @@
 ﻿using AccesoDatos;
 using EntidadesDirectorio;
 using System;
-using System.Collections.Generic;
 using System.Data;
 
 namespace CapaLogica
 {
     public class LogicaUsuario
     {
-        
+
 
         public DataTable listaUsuarios(string condition)
         {
@@ -25,9 +24,28 @@ namespace CapaLogica
             }
 
             return lista;
-
-
         }
 
+
+        public int IngresarUsuario(ClsUsuarios pvo_Usuario, ClsUbicaciones pvo_Ubicaciones, ClsOcupaciones pvo_Ocupaciones, ClsOcupacionesProfesionales pvo_OcupacionesProfesionales, ClsWebSites pvo_Sitio)
+        {
+            //Variables
+            int vln_Correcto = 0;
+
+            //Inicio
+            AccesoDatosUsuario vlo_ADUsuario = new AccesoDatosUsuario();
+
+            try
+            {
+                vln_Correcto = vlo_ADUsuario.InsertarUsuario(pvo_Usuario, pvo_Ubicaciones, pvo_Ocupaciones, pvo_OcupacionesProfesionales, pvo_Sitio);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return vln_Correcto;
+        }
     }
 }
