a94e280e04d344bed8a48b566f6b032396e25e34
cheveztomas <42824002+cheveztomas@users.noreply.github.com>
2019-01-10 15:40:57 -0600

    Varios

--- DirectorioServicios/FrmRegistrar.aspx.designer.cs
+++ DirectorioServicios/FrmRegistrar.aspx.designer.cs
@@ -11,5 +11,77 @@ namespace DirectorioServicios {
     
     
     public partial class FrmRegistrar {
+        
+        /// <summary>
+        /// Control txt_Correo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_Correo;
+        
+        /// <summary>
+        /// Control txt_Conrteasenia.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_Conrteasenia;
+        
+        /// <summary>
+        /// Control txt_ConrteaseniaConfir.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_ConrteaseniaConfir;
+        
+        /// <summary>
+        /// Control txt_Nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox txt_Nombre;
+        
+        /// <summary>
+        /// Control Txt_Apellido1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Txt_Apellido1;
+        
+        /// <summary>
+        /// Control Txt_Apellido2.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Txt_Apellido2;
+        
+        /// <summary>
+        /// Control Txt_Telefono.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Txt_Telefono;
+        
+        /// <summary>
+        /// Control Txt_Descipcion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox Txt_Descipcion;
     }
 }
