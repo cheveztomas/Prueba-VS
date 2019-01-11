fcfa02d5bd5048a70916f95b7f8ba6883c8666ff
cheveztomas <42824002+cheveztomas@users.noreply.github.com>
2019-01-11 11:53:22 -0600

    Update JavaScript.js

--- DirectorioServicios/App/js/JavaScript.js
+++ DirectorioServicios/App/js/JavaScript.js
@@ -1,1 +1,9 @@
-﻿
+﻿function ValidarPassword(){
+	//Variables
+	var pass1="";
+	var pass2="";
+
+	//Inicio
+	pass1=document.getElementById("txt_Conrteasenia").value;
+	pass1=document.getElementById("txt_ConrteaseniaConfir").value;
+}
