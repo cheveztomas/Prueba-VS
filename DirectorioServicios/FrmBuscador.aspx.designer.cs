822207b212625a84dff546f927561734aced3f37
Juan Castro <sempai211@gmail.com>
2019-01-10 23:11:58 -0600

    Update FrmBuscador.aspx.designer.cs

--- DirectorioServicios/FrmBuscador.aspx.designer.cs
+++ DirectorioServicios/FrmBuscador.aspx.designer.cs
@@ -56,5 +56,14 @@ namespace DirectorioServicios {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnBuscar;
+        
+        /// <summary>
+        /// Control lista.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl lista;
     }
 }
