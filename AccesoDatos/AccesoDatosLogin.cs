2e9328aec3f3b68b5b620b07910653d1324f2652
cheveztomas <42824002+cheveztomas@users.noreply.github.com>
2019-01-10 19:16:55 -0600

    Update AccesoDatosLogin.cs

--- AccesoDatos/AccesoDatosLogin.cs
+++ AccesoDatos/AccesoDatosLogin.cs
@@ -72,10 +72,11 @@ namespace AccesoDatos
                 };
                 string vlc_SentenciaPagina = string.Empty;
 
-                vlc_SentenciaPagina = "call PAGINA_WEB.SP_REGISTRAR_Y_ACTUALIZAR_USUARIO(?,?,?,?,?,?,?,?,?,?);";
+                vlc_SentenciaPagina = "SP_REGISTRAR_Y_ACTUALIZAR_USUARIO";
 
-                CommandPagina.Parameters["_ID_USUARIO"].Direction = ParameterDirection.InputOutput;
-                CommandPagina.Parameters.AddWithValue("_ID_USUARIO", pvo_Usuario.ID_Usuario);
+                CommandPagina.CommandType = CommandType.StoredProcedure;
+                CommandPagina.Parameters.AddWithValue("@_ID_USUARIO", pvo_Usuario.ID_Usuario);
+                CommandPagina.Parameters["@_ID_USUARIO"].Direction = ParameterDirection.InputOutput;
                 CommandPagina.Parameters.AddWithValue("_NOMBRE", pvo_Usuario.Nombre_Profesional);
                 CommandPagina.Parameters.AddWithValue("_APELLIDO1", pvo_Usuario.Apellido1_Profesional);
                 CommandPagina.Parameters.AddWithValue("_APELLIDO2", pvo_Usuario.Apellido2_Profesional);
@@ -86,9 +87,7 @@ namespace AccesoDatos
                 CommandPagina.Parameters.AddWithValue("_ES_PROFESIONAL", pvo_Usuario.Perfil_Profesional);
                 CommandPagina.Parameters.Add("@_MSJ", MySqlDbType.VarChar, 100);
                 CommandPagina.Parameters["@_MSJ"].Direction = ParameterDirection.Output;
-                CommandPagina.CommandType = CommandType.StoredProcedure;
                 CommandPagina.CommandText = vlc_SentenciaPagina;
-
 
                 ConexionOagina.Open();
                 vln_Correcto = CommandPagina.ExecuteNonQuery();
